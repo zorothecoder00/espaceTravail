12a47f682d49a047c9dd99d3a6f9dc98f54465af
zorothecoder00 <awledouamoussou95@gmail.com>
2025-08-18 15:38:30 +0000

    Réduction de la colonne etat et priorite

--- src/app/interfaceUtilisateur/planning/vue/page.tsx
+++ src/app/interfaceUtilisateur/planning/vue/page.tsx
@@ -132,12 +132,12 @@ export default function CalendrierPage() {
             <table className="w-full text-sm table-fixed">
               <colgroup>
                 <col className="w-10" />
-                <col className="w-32" />
-                <col className="w-28" />
-                <col className="w-32" />
+                <col className="w-35" />
+                <col className="w-31" />
+                <col className="w-35" />
 
-                <col className="w-20" />
-                <col className="w-20" />
+                <col className="w-15" />
+                <col className="w-15" />
                 <col className="w-50" />
               </colgroup>
               <thead className="bg-gray-50 text-left">
